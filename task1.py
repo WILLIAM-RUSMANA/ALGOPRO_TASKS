1ac0c29dd7d5a02c89228d6d930b171c871e67d7
WILLIAM-RUSMANA <93782439+WILLIAM-RUSMANA@users.noreply.github.com>
2024-09-18 09:42:27 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -10,7 +10,6 @@ my_num = int(math.floor((random.random() * 100)+1))
 
 while loop:
     player_num = int(input(": "))
-    print(my_num)
     if my_num == player_num:
         print(f"Yay we picked the same number, {my_num}")
         loop = False
