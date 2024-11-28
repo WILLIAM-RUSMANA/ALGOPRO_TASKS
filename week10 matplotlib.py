9c27e055ef0afa7b2ad681d90040eb56cc474a5e
WILLIAM ER <93782439+WILLIAM-RUSMANA@users.noreply.github.com>
2024-11-28 20:13:41 +0700

    Update week10 matplotlib.py

--- week10 matplotlib.py
+++ week10 matplotlib.py
@@ -5,8 +5,8 @@ import numpy as np
 def power2(n):
     return n**2
 
-array_size = int(input())
-plot_style = input()
+array_size = int(input("Input array size: "))
+plot_style = input("Input plot style scatter, bar or line")
 array_list = [i for i in range(array_size-1)]
 
 xpoints = np.array([array_list])
@@ -15,7 +15,7 @@ ypoints = [i**2 for i in array_list]
 if plot_style == "scatter":
     plt.scatter(xpoints, ypoints)
 elif plot_style == "bar":
-    catergories = input("categories").split()
+    catergories = input("categories: ").split()
     val = input("values: ").split()
     plt.bar(catergories, val)
 else:
